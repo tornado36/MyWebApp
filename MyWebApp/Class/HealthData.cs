d849def951bbcd2e18547c118e0606959f19f0fe
tornado36 <514019326@qq.com>
2020-09-27 18:29:20 +0800

    feat(health-data): add health data class

---
+++ MyWebApp/Class/HealthData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Class
+{
+    public class HealthData
+    {
+        public string ProjectID { get; set; }
+        public DateTime UTCTIme { get; set; } = DateTime.UtcNow;
+        public double EnergyInput { get; set; }
+        public int Steps { get; set; }
+        public double BodyWeight { get; set; }
+    }
+}
